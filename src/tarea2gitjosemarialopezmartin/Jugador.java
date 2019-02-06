e11670967eb90bd28e80916f6b4889a5f5b8dba1
wantadog <jlopmar196@gmail.com>
2019-02-06 14:18:03 +0100

    Añade getters y setters

--- src/tarea2gitjosemarialopezmartin/Jugador.java
+++ src/tarea2gitjosemarialopezmartin/Jugador.java
@@ -19,4 +19,30 @@ public class Jugador {
         this.votos = votos;
         this.id = id;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
 }
