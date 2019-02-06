a4a7466825fb4eea109f8993dc032d0a158a2fc7
wantadog <jlopmar196@gmail.com>
2019-02-06 14:34:00 +0100

    Añade toString

--- src/tarea2gitjosemarialopezmartin/Jugador.java
+++ src/tarea2gitjosemarialopezmartin/Jugador.java
@@ -43,6 +43,10 @@ public class Jugador {
     public void setId(int id) {
         this.id = id;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", votos=" + votos + ", id=" + id + '}';
+    }
     
 }
